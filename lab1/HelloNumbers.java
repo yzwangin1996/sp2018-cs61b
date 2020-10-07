d7f2c463b7f8574bb6dc3cef69e57260630a55a0
Yingzi.Wang <Yingzi.Wang@target.com>
2020-10-07 13:29:51 -0500

    finish lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,18 @@
+public class HelloNumbers {
+  public static void main(String[] args) {
+    int x;
+    x = 0;
+    while (x < 10) {
+      System.out.print(x);
+      x = x + 1;
+    }
+  }
+}
+
+
+/*
+1. Jva variables must be declared before using
+2. Java variables must have a speific type
+3. Java variable types can never change
+4. Types are verified before code even runs
+*/
---
+++ lab1/HelloWord.java
@@ -0,0 +1,12 @@
+public class HelloWord {
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
+}
+
+/**
+1. In java, all code must be part of a class
+2. Classes are defined with "public class CLASSNAME"
+3. We use {} to delineate the beginning and ending of things
+4. We must end lines with a semicolon
+5. The code we want to run must be inside public static void main(String[] args) **/
